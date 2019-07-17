27cfa4d2d7653ac08c7cb2588b463dc309f657af
NoureldeanSaed <noureldean.saed@gmail.com>
2019-07-17 20:46:49 +0300

    saving work

--- src/containers/ContactUs/index.js
+++ src/containers/ContactUs/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { Dimensions } from 'react-native'
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5'
 
